1fc9d19a59e470d4e4765132fc99c43b67469de7
ankit2103 <achawda866@gmail.com>
2021-12-26 18:29:09 +0530

    Update

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -59,7 +59,6 @@ router.get('/:id',(req,res,next)=>{
 
 //Delete User
 router.delete('/:id',(req,res,next)=>{
-    // Student.findById(req.params.id)
     Student.deleteOne({_id:req.params.id})
     .then(result=>{
         res.status(200).json({
@@ -72,6 +71,27 @@ router.delete('/:id',(req,res,next)=>{
     })
 })
 
+
+//Put Request
+router.put('/:id',(req,res,next)=>{
+    Student.findOneAndUpdate({_id:req.params.id},{
+        $set:{
+            name:req.body.name,
+            email:req.body.email,
+            phone:req.body.phone,
+            gender:req.body.gender
+        }
+    })
+    .then(result=>{
+        res.status(200).json({
+            message:'updated successfully'
+        });
+    }).catch(err=>{
+        res.status(500).json({
+            message:"Something went worng"
+        })
+    })
+})
 
 
 module.exports = router
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express    = require('express');
 const app        = express();
 const route      = require('./api/routes/routes');
 const faculty    = require('./api/routes/faculty');
